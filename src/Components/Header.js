6cce421de609856fa9f241fd4801d8304d5843a2
asad-mahmood1 <30564031+asad-mahmood1@users.noreply.github.com>
2023-11-14 21:00:26 -0500

    Update Header.js with New Header Text and No Logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,9 +5,7 @@ import HeaderImage from '../../public/OD_Logo.png';
 function Header() {
   return (
     <div className="Header">
-      <img className="Header_Image" src={HeaderImage} loading="lazy" />
-      <span className='Header_Text'>OPERATION DRAGONFLY</span>
-      <a className='Header_Profile'>Profile</a>
+      <span className='Header_Text'>OPERATION SHEPHERD</span>
     </div>
   );
 }
